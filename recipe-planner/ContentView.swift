79f378ac484d24017bcee674c1b99ba46cd2bd12
Dan <saturndan@protonmail.com>
2023-02-01 14:21:12 +1100

    Initial Commit

---
+++ recipe-planner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  recipe-planner
+//
+//  Created by Dan Malyaris on 1/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe-planner/recipe_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_plannerApp.swift
+//  recipe-planner
+//
+//  Created by Dan Malyaris on 1/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct recipe_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
